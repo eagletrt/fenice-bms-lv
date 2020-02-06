4d3f1248758b82b8c05255212f4e3315f6c61d11
lucacaronti <carontiluca@gmail.com>
2020-02-06 19:14:08 +0100

    added some ltc functions

--- bms_LV/Src/ltc.c
+++ bms_LV/Src/ltc.c
@@ -24,10 +24,31 @@ void read_temperatures()
 {
 }
 
-void ltc6813_wakeup_idle(SPI_HandleTypeDef *hspi)
+void ltc6810_wakeup_idle(SPI_HandleTypeDef *hspi)
 {
     uint8_t data = 0xFF;
-    HAL_GPIO_WritePin(CS_LTCx, CS_LTCn, GPIO_PIN_RESET);
+    spi_enable_cs(hspi);
     HAL_SPI_Transmit(hspi, &data, 1, 1);
-    HAL_GPIO_WritePin(CS_LTCx, CS_LTCn, GPIO_PIN_SET);
+    spi_disable_cs(hspi);
+}
+void spi_enable_cs(SPI_HandleTypeDef *spi) {
+	HAL_GPIO_WritePin(CS_LTCx, CS_LTCn, GPIO_PIN_RESET);
+	while (spi->State != HAL_SPI_STATE_READY);
+}
+
+void spi_disable_cs(SPI_HandleTypeDef *spi) {
+	while (spi->State != HAL_SPI_STATE_READY);
+	HAL_GPIO_WritePin(CS_LTCx, CS_LTCn, GPIO_PIN_SET);
+}
+
+uint16_t _pec15(uint8_t len, uint8_t data[]) {
+	uint16_t remainder, address;
+	remainder = 16;  // PEC seed
+	for (int i = 0; i < len; i++) {
+		// calculate PEC table address
+		address = ((remainder >> 7) ^ data[i]) & 0xff;
+		remainder = (remainder << 8) ^ crcTable[address];
+	}
+	// The CRC15 has a 0 in the LSB so the final value must be multiplied by 2
+	return (remainder * 2);
 }
--- bms_LV/build/pid_controller.d
+++
@@ -1,3 +0,0 @@
-build/pid_controller.o: Src/pid_controller.c Inc/pid_controller.h
-
-Inc/pid_controller.h:
