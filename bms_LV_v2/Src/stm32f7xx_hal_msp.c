e0507bab2c30a15ab6959b1c3eb62e93e8bf74f7
lucacaronti <carontiluca@gmail.com>
2020-02-27 14:16:21 +0100

    set MISO internal pull-up

--- bms_LV_v2/Src/stm32f7xx_hal_msp.c
+++ bms_LV_v2/Src/stm32f7xx_hal_msp.c
@@ -234,7 +234,14 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
     PB15     ------> SPI2_MOSI
     PD3     ------> SPI2_SCK 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
+    GPIO_InitStruct.Pin = GPIO_PIN_14;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_15;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
