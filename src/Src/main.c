576e6a903051ff8b99a8bed66380b5dd4176e3c6
Simone Ruffini <simone.ruffini@tutanota.com>
2021-03-26 12:41:34 +0100

    update frimware 1.16.1 and CubeMX 6.2.0

--- src/Src/main.c
+++ src/Src/main.c
@@ -527,7 +527,7 @@ static void MX_CAN1_Init(void)
   hcan1.Init.Prescaler = 3;
   hcan1.Init.Mode = CAN_MODE_NORMAL;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_15TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_16TQ;
   hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
   hcan1.Init.AutoBusOff = DISABLE;
@@ -564,7 +564,7 @@ static void MX_CAN3_Init(void)
   hcan3.Init.Prescaler = 3;
   hcan3.Init.Mode = CAN_MODE_NORMAL;
   hcan3.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan3.Init.TimeSeg1 = CAN_BS1_15TQ;
+  hcan3.Init.TimeSeg1 = CAN_BS1_16TQ;
   hcan3.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan3.Init.TimeTriggeredMode = DISABLE;
   hcan3.Init.AutoBusOff = DISABLE;
