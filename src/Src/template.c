bcf4d7dfd492d83ec5e676a2302b60c54c1c51f9
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2021-07-08 13:20:57 +0200

    added templates

---
+++ src/Src/template.c
@@ -0,0 +1,19 @@
+/**
+ * @file      template_name.c
+ * @author    Author Name [author.name@mailprovider.xxx]
+ * @date      DATE
+ * @updated   revisions
+ * @ingroup  i don't know yet how to use this
+ * @prefix   TEMPLATE
+ * 
+ * @brief     
+ * 
+ */
+/* Includes ------------------------------------------------------------------*/
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
